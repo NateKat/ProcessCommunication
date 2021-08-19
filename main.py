417630a5bf00a29f5c38ab35d2e498eaa816fdd2
Netanel Katzburg <netanel10k@gmail.com>
2021-08-19 14:55:05 +0300

    Add data vector generation and requirements.txt
    Currently  requires numpy package for calculations and data generation.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import numpy as np
+
+
+def data_vector_gen(seed=100, vector_size=50):
+    """
+    Generator of normal (uniform Gaussian distribution) vector
+    :param seed: seed for random
+    :param vector_size: vector size: (1 x vector_size)
+    :return: yields the next vector for each call
+    """
+    np.random.RandomState(seed)
+    while True:
+        yield np.random.normal(size=vector_size)
 
 
 if __name__ == '__main__':
