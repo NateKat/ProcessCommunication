3a164f23c5644c53b27b66fa06b2ca450a79a53f
Netanel Katzburg <netanel10k@gmail.com>
2021-08-26 17:25:30 +0300

    analyse client: get incoming data with asyncio and split to producer and consumer

--- analyse_client.py
+++ analyse_client.py
@@ -4,6 +4,7 @@ from numpysocket import NumpySocket
 from decorators import timer, ThrottleDecorator
 from time import sleep
 import json
+import asyncio
 from logger import create_logger
 
 logger = create_logger()
@@ -41,13 +42,45 @@ class AnalyseClient(CommunicationProc):
                 sleep(1)
                 continue
 
-        for i in range(20):
-            self.data_handler()
+        asyncio.run(self.get_incoming_data())
         try:
             self.np_socket.close()
         except OSError:
             logger.error("server already disconnected")
         self.finalize_and_save_data()
+
+    async def get_incoming_data(self, num_producers: int = 1, num_consumers: int = 4) -> None:
+        queue = asyncio.Queue()
+
+        # Fire up producers and consumers
+        producers = [asyncio.create_task(self.producer(queue)) for _ in range(num_producers)]
+        consumers = [asyncio.create_task(self.consumer(queue)) for _ in range(num_consumers)]
+
+        await asyncio.gather(*producers)  # wait for the producers to finish
+        await queue.join()  # wait for the remaining tasks to be processed
+
+        # Cancel the consumers, which are now idle
+        for c in consumers:
+            c.cancel()
+
+    @timer
+    def get_batch_frames(self, num_of_matrices: int) -> list:
+        return [self.get_frames_in_matrix() for _ in range(num_of_matrices)]
+
+    async def producer(self, queue: asyncio.queues.Queue, matrix_batch_size: int = 10) -> None:
+        for _ in range(20):
+            frames_list, time = self.get_batch_frames(matrix_batch_size)
+            self.receive_rates.append(self.columns_in_matrix * matrix_batch_size / time)
+            print(f"Receive frequency:{self.current_freq:.2f}[Hz]")
+            for matrix in frames_list:
+                await queue.put(matrix)
+
+    async def consumer(self, queue: asyncio.queues.Queue) -> None:
+        while True:
+            token = await queue.get()
+            self.save_matrix(token)
+            queue.task_done()
+            await asyncio.sleep(0.1)
 
     def finalize_and_save_data(self):
         keys = ['mean', 'standard deviation']
@@ -56,38 +89,21 @@ class AnalyseClient(CommunicationProc):
         with open('data.json', 'w', encoding='utf-8') as f:
             json.dump(self.data_dict, f, ensure_ascii=False, indent=4)
 
-    def data_handler(self):
-        for i in range(10):
-            self.matrix_handler()
-        print(f"Receive frequency:{self.current_freq:.2f}[Hz]")
-
-    @call_times_in_seconds(1000, 1)
     def receive_vector(self) -> bytearray:
         return self.np_socket.receive_vector_frame(16)
-
-    @timer
-    def accumulate_frames(self, num_of_frames: int) -> list:
-        return [self.receive_vector() for _ in range(num_of_frames)]
 
     def frames_to_matrix(self, l_frames: list) -> np.ndarray:
         return np.vstack([self.np_socket.frame_to_vector(frame) for frame in l_frames])
 
-    def get_matrix(self) -> np.ndarray:
-        l_frames, time = self.accumulate_frames(self.columns_in_matrix)
-        logger.debug(f"Received {self.columns_in_matrix} vectors in {time}  seconds")
-        self.receive_rates.append(self.columns_in_matrix / time)
-        matrix = self.frames_to_matrix(l_frames)
-        logger.debug(f"matrix received: {matrix}")
-        return matrix
+    def get_frames_in_matrix(self) -> list:
+        return [self.receive_vector() for _ in range(self.columns_in_matrix)]
 
     def matrix_analytics(self, matrix: np.ndarray) -> tuple:
         return np.mean(matrix, axis=0), np.std(matrix, axis=0)
 
-    def save_matrix(self, matrix: np.ndarray) -> None:
+    def save_matrix(self, l_frames: list) -> None:
+        matrix = self.frames_to_matrix(l_frames)
+        logger.debug(f"matrix received: {matrix}")
         keys = ['matrix', 'mean', 'standard deviation']
         values = [arr.tolist() for arr in [matrix, *self.matrix_analytics(matrix)]]
         self.data_dict['matrices'].append(dict(zip(keys, values)))
-
-    def matrix_handler(self) -> None:
-        self.save_matrix(self.get_matrix())
-
